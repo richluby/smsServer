40f24c382f03faf91a3a6f6d315f4c6a90cd92de
Richard Luby <RLuby@Debian8.1-Phil>
2015-10-24 10:25:25 -0400

    server uses configuration dictionary instead of global variables

--- sendSMSServer.py
+++ sendSMSServer.py
@@ -4,78 +4,76 @@ import sys, getopt, SocketServer
 from ThreadingUDPServer import ThreadingUDPHandler, ThreadingUDPServer
 from Packet import *
 
+#the configuration dictionary
+_CONF_DICT = {}
 # the port on which to listen
-_PORT = 8658
+_CONF_DICT["port"] = 8658
 # the configuration file to read
 # most configuration files go in /etc on a Linux system
-_CONF_FILE = "smsServer.conf"
+_CONF_DICT["confFile"] = "smsServer.conf"
 # the number of seconds to keep a number in memory
 # default is 1800 (30min)
-_TIMEOUT = 1800
+_CONF_DICT["timeout"] = 1800
 # determines if the server should fork a back ground process or not
-_BACKGROUND = False
+_CONF_DICT["background"] = False
 # the set of numbers known to the server at any given time
 numberSet = set()
 
 # reads the configuration file and populates the correct fields
 def readConfiguration():
 	lines = []
-	global _PORT
-	global _TIMEOUT
-	global _BACKGROUND
+	global _CONF_DICT
 	try:
-		with open(_CONF_FILE) as f:
+		with open(_CONF_DICT["confFile"]) as f:
 			lines = f.readlines()
 	except IOError:
-		print "ERROR: Failed to parse", _CONF_FILE,". Proceeding with current configuration."
+		print "ERROR: Failed to parse", _CONF_DICT["confFile"],". Proceeding with current configuration."
 		return 0
 	tokens = []
 	for line in lines:
 		tokens = line.split(" ")
 		if (tokens[0] == "Timeout"):
-			_TIMEOUT = int(tokens[1])
+			_CONF_DICT["timeout"] = int(tokens[1])
 		elif (tokens[0] == "Port"):
-			_PORT = int(tokens[1])
+			_CONF_DICT["port"] = int(tokens[1])
 		elif (tokens[0] == "Background"):
-			_BACKGROUND = bool(tokens[1])
+			_CONF_DICT["background"] = bool(tokens[1])
 
 
 # prints the help and then exits
 def printHelp():
 	print "\nOctober 2015 \n\
 	Creates a server that sends SMS messages to specified phone numbers. \n\
-	Default configuration file is in" , (_CONF_FILE), ".\n"
+	Default configuration file is in" , (_CONF_DICT["confFile"]), ".\n"
 	print "	-h		prints this help, and then exits\n\
 	-b		forks the server into the background\n\
 	-p <number>	sets the port on which to listen\n\
 	-c <file>	sets the configuration file\n\
 	-t <seconds>	sets the timeout in seconds to keep a number in \n\
-			memory. Default is", (_TIMEOUT), "."
+			memory. Default is", (_CONF_DICT["timeout"]), "."
 	exit(0)
 
 # parses the command line arguments
 def parseArgs():
 	args, opts = getopt.gnu_getopt(sys.argv[1:], "p:c:t:hb",[""])
-	global _PORT
-	global _TIMEOUT
-	global _BACKGROUND
+	global _CONF_DICT
 	for arg, opt in args:
 		if (arg == "-h"):
 			printHelp()
 		elif (arg == "p"):
-			_PORT = int(opt)
+			_CONF_DICT["port"] = int(opt)
 		elif (arg == "c"):
-			_CONF_FILE = opt
+			_CONF_DICT["confFile"] = opt
 		elif (arg == "t"):
-			_TIMEOUT = int(opt)
+			_CONF_DICT["timeout"] = int(opt)
 
 # starts a server on PORT that listens for UDP packets
 def setupServer():	
-	server = ThreadingUDPServer(("127.0.0.1", _PORT), ThreadingUDPHandler)
+	server = ThreadingUDPServer(("127.0.0.1", _CONF_DICT["port"]), ThreadingUDPHandler)
 	server.serve_forever()
 
 if __name__ == "__main__":
 	parseArgs()	
 	readConfiguration()
-	print "Starting SMS Server on port", _PORT,"..."
+	print "Starting SMS Server on port", _CONF_DICT["port"],"..."
 	setupServer()
