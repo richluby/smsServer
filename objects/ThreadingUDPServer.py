552e2be402da6dbea39114c96c69110262181de0
Richard Luby <RLuby@Debian8.1-Phil>
2015-10-29 13:49:04 -0400

    added ability to receive and parse packets added test cases to verify server receives packets

--- objects/ThreadingUDPServer.py
+++ objects/ThreadingUDPServer.py
@@ -1,11 +1,64 @@
 #!/usr/bin/python
 
-import SocketServer
+import SocketServer, logging, socket
+from objects import *
+from Packet import *
+from collections import OrderedDict
+
+logging.basicConfig(level=logging.INFO, format="%(name)s: %(message)s")
 
 class ThreadingUDPHandler(SocketServer.BaseRequestHandler):
+# handles receiving data
+	def __init__(self, request, client_address, server):
+	# initializes the logger
+		self.logger = logging.getLogger("UDPHandler")
+		SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
+		return
+
 	# handles incoming items
 	def handle(self):
 		data = self.request[0].strip()
-		print "I saw ", data, "from", self.client_address[0]
+		packet = Packet.decryptPacket(data)
+		self.server.handlePacket(packet, self.client_address)
+		self.logger.debug("I saw %s from %s", packet, self.client_address[0])
 
-class ThreadingUDPServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer): pass
+class ThreadingUDPServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer): 
+# serves the sms server
+	# defines the maximum number of ACK packets to check in memory
+	MAX_ACKS = 50
+
+	def __init__(self, serverAddress, handler):
+	# initializes the logging interface
+		self.logger = logging.getLogger("Server")
+		self.logger.info("Initializing server for %s.", serverAddress)
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.packetDict = OrderedDict()
+		SocketServer.UDPServer.__init__(self, serverAddress, handler)
+
+	def handleNumberPacket(self, packet):
+		self.logger.info("NumberPacket: %s", packet)
+	
+	def handleNotifyPacket(self, packet):
+	# sends a notification to the given number
+		self.logger.info("NotifyPacket: %s", packet)
+	
+	def handleClientPacket(self, packet):
+	# handles a client packet
+		self.logger.info("ClientPacket: %s", packet)
+
+	def handlePacket(self, packet, clientAddress):
+	# handles parsing an individual packet
+		if isinstance(packet, NumberPacket):
+			self.handleNumberPacket(packet)
+		elif isinstance(packet, NotifyPacket):
+			self.handleNotifyPacket(packet)
+		elif isinstance(packet, ClientPacket):
+			self.handleClientPacket(packet)
+		if (packet.seqNum not in self.packetDict):
+			self.sock.sendto(packet.packedBytes, clientAddress) 
+	
+	def close(self):
+	# closes the socket associated with the server
+		self.socket.close()
+		self.sock.close()
+		self.logger.info("Server closed.")
---
+++ tests/test_ServerModule.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import unittest, socket
+import threading
+from objects import *
+
+class ServerBaseClass(unittest.TestCase):
+# creates a server instance against which to check
+	@classmethod
+	def setUpClass(self):
+	# creates a socket through which to send information
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.serverAddress = ("localhost", 8657)
+		self.server = ThreadingUDPServer(self.serverAddress, ThreadingUDPHandler)
+		serverThread = threading.Thread(target=self.server.serve_forever)
+		serverThread.setDaemon(True)
+		serverThread.start()
+	
+	@classmethod
+	def tearDownClass(self):
+	# closes the socket
+		self.sock.close()
+		self.server.close()
+		
+class TestServerModule(ServerBaseClass):
+# tests the server Module
+	def test_numberPacketReceiver(self):
+	# tests buidling the number packet
+		packet = NumberPacket()
+		packet.options.addNumber = True
+		self.sock.sendto(packet.packedBytes, self.serverAddress)
+		received = self.sock.recv(1024)
+		packet = Packet.parseBytes(received)
+		self.assertIsInstance(packet, NumberPacket)
+
+	def test_clientPacketReceiver(self):
+	# tests buidling the client packet
+		packet = ClientPacket()
+		packet.options.addClient = True
+		self.sock.sendto(packet.packedBytes, self.serverAddress)
+		received = self.sock.recv(1024)
+		packet = Packet.parseBytes(received)
+		self.assertIsInstance(packet, ClientPacket)
+
+	def test_notifyPacketReceiver(self):
+	# tests buidling the notify packet
+		packet = NotifyPacket()
+		packet.options.sendSMS = True
+		self.sock.sendto(packet.packedBytes, self.serverAddress)
+		received = self.sock.recv(1024)
+		packet = Packet.parseBytes(received)
+		self.assertIsInstance(packet, NotifyPacket)
